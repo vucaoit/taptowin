bc56aae9667a254b0bb07abc479468b24f31feee
vucaoit <vucaoit@gmail.com>
2021-08-05 22:00:03 +0700

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  SystemChrome.setEnabledSystemUIOverlays([]);
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -29,85 +27,159 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  double count = 0;
+  bool gameover = false;
+  late AnimationController _controller;
+  @override
+  void initState() {
+    // TODO: implement initState
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 5000),
+      vsync: this,
+    );
+    super.initState();
+  }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final height = MediaQuery.of(context).size.height;
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+        body: Center(
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: height,
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            RotationTransition(
+              turns: AlwaysStoppedAnimation(180 / 360),
+              child: InkWell(
+                onTap: () {
+                  setState(() {
+                    if (count < (height - 200) / 2 && gameover == false)
+                      count += (height - 200) / 20;
+                    else
+                      {
+                        gameover = true;
+                        _controller.repeat();
+                      }
+                  });
+                },
+                child: Container(
+                  alignment: Alignment.center,
+                  color: Colors.cyan,
+                  width: MediaQuery.of(context).size.width,
+                  height: 100,
+                  child: Text(
+                    "TAP HERE",
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: (height - 200),
+              child: Column(
+                children: [
+                  Container(
+                    color: Colors.yellow,
+                    width: MediaQuery.of(context).size.width,
+                    height: ((height - 200) / 2 + count > 0)
+                        ? ((height - 200) / 2 + count)
+                        : 0,
+                    child: (gameover && count > 0)
+                        ? FlatButton(
+                            onPressed: () {
+                              setState(() {
+                                reset();
+                              });
+                            },
+                            child: RotationTransition(
+                              turns: new AlwaysStoppedAnimation(180 / 360),
+                              child: customRotation("YELLOW"),
+                            ),
+                          )
+                        : Text(""),
+                  ),
+                  Expanded(
+                      child: Container(
+                    color: Colors.red,
+                    width: MediaQuery.of(context).size.width,
+                    height: ((height - 200) / 2 + count >= height - 200)
+                        ? 0
+                        : (height - 200) / 2,
+                    child: (gameover && count < 0)
+                        ? FlatButton(
+                            onPressed: () {
+                              setState(() {
+                                reset();
+                              });
+                            },
+                            child: customRotation("RED"),
+                            )
+                        : Text(""),
+                  ))
+                ],
+              ),
+            ),
+            InkWell(
+              onTap: () {
+                setState(() {
+                  if (count > (height - 200) / 2 * -1 && gameover == false)
+                    count -= (height - 200) / 20;
+                  else
+                    gameover = true;
+                });
+              },
+              child: Container(
+                alignment: Alignment.center,
+                color: Colors.cyan,
+                width: MediaQuery.of(context).size.width,
+                height: 100,
+                child: Text(
+                  "TAP HERE",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    ));
+  }
+
+  reset() {
+    this.gameover = false;
+    this.count = 0;
+  }
+  Widget customRotation(String name){
+    return RotationTransition(
+      turns: Tween(begin: 0.0, end: 1.0).animate(_controller),
+      child: Text(
+        "$name WIN\nRESTART",
+        style: TextStyle(
+            color: Colors.white,
+            fontSize: 50,
+            fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
